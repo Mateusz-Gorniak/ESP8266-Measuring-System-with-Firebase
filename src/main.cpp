c5e06c3e7049859730a4b451a7be3cce6fe25c58
Mateusz-Gorniak <mateusz.gorniak@student.wat.edu.pl>
2021-11-25 16:11:20 +0100

    wifi connection setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
+//includes
 #include <Arduino.h>
+#include <stdio.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+//defines
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  //Initialaize Serial
+  Serial.begin(115200);
+  //Initialize Wi-Fi
+  WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  //Checking the Wi-Fi connection 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.getHostname());
+
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 }
