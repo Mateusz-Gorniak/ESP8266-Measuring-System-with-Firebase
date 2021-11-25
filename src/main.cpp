7ee9fce1811ac23f1a20e5f7374c8b1569e27fb4
Mateusz-Gorniak <mateusz.gorniak@student.wat.edu.pl>
2021-11-25 17:03:23 +0100

    Firebase instances add

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,30 @@
 #include <Arduino.h>
 #include <stdio.h>
 #include <ESP8266WiFi.h>
+#include <Firebase_ESP_Client.h>
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
 
 //defines
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
+#define EMAIL ""
+#define PASSWORD ""
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyCb8ebThdsGcFKk0XHxxmYez177m-a-pXA"
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://esp-firebase-1903d-default-rtdb.europe-west1.firebasedatabase.app/"
+
+//Firebase Data Objects
+FirebaseData data; 
+FirebaseAuth auth;
+FirebaseConfig config; 
+
+//variables 
+bool isFirebaseConnected; 
 
 void setup()
 {
